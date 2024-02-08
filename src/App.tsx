93be6eb8c5b4a32eabe3eae6ee81dadd18a17441
mburhanudin <muhammadburhanudin@refactory.id>
2024-02-08 13:29:51 +0700

    setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Blog from './pages/Blog';
-import Denom from './pages/Denom';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Blog from "./pages/Blog";
+import Denom from "./pages/Denom";
 
-const ProtectedRoute: React.FC<{ element: React.ReactElement }> = ({ element }) => {
-  const token = sessionStorage.getItem('token');
+const ProtectedRoute: React.FC<{ element: React.ReactElement }> = ({
+  element,
+}) => {
+  const token = sessionStorage.getItem("token");
 
   if (!token) {
     return <Navigate to="/login" />;
@@ -16,26 +23,16 @@ const ProtectedRoute: React.FC<{ element: React.ReactElement }> = ({ element }) 
 };
 
 const App: React.FC = () => {
+  const token = sessionStorage.getItem('token');
   return (
     <Router>
       <Routes>
-        <Route
-          path="/login"
-          element={<Login />}
-        />
-        <Route
-          path="/register"
-          element={<Register />}
-        />
-        {/* Gunakan ProtectedRoute untuk /blog */}
-        <Route
-          path="/blog"
-          element={<ProtectedRoute element={<Blog />} />}
-        />
-        <Route
-          path="/denom"
-          element={<ProtectedRoute element={<Denom />} />}
-        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/blog" element={<ProtectedRoute element={<Blog />} />} />
+        <Route path="/denom" element={<ProtectedRoute element={<Denom />} />} />
+
+        <Route path="/" element={<Navigate to={token ? '/blog' : '/login'} />}/>
       </Routes>
     </Router>
   );
