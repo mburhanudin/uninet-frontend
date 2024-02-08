acd31f847ad7d07c8bd452756aa47345a1bf13b6
mburhanudin <muhammadburhanudin@refactory.id>
2024-02-08 12:58:30 +0700

    remove unused code

--- src/components/templates/MainLayout.tsx
+++ src/components/templates/MainLayout.tsx
@@ -1,6 +1,6 @@
 // components/templates/MainLayout.tsx
 import React, { useState } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
@@ -12,7 +12,6 @@ interface MainLayoutProps {
 }
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
-  const navigate = useNavigate();
   const location = useLocation();
   const [isSidebarOpen, setSidebarOpen] = useState(false);
 
